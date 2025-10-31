89555f5b4eba562c749918f5d4a1a5fc227cae5e
kylian.drif <kylian.drif@epita.fr>
2025-10-31 22:29:10 +0100

    fix(chap2ex1): changing the unhide step number

--- lessons/chap2exo1.js
+++ lessons/chap2exo1.js
@@ -258,7 +258,7 @@ function unhideThirdPart() {
 
 function unhideFourthPart() {
     UpdateSuccess(7);
-    const part = document.getElementById('thirdPart');
+    const part = document.getElementById('fourthPart');
     part.classList.remove("hide");
 }
 
